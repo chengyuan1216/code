60489fadc6d8e7c1fedcbe6ac65ad3e14c6eb1cb
liuchengyuan <2267698673@qq.com>
2019-12-17 07:38:10 +0800

    提交代码

---
+++ uniapp/myuniapp/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		onError: function(err) {
+			console.error('App onError',err)
+		},
+		onUniNViewMessage: function() {
+			console.log('App onUniNViewMessage')
+		},
+		globalData: {
+			test: 'asd'
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
