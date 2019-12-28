0a74114b03a48720932271200cd6042021957147
liuchengyuan <liucy@rp-pet.cn>
2019-12-28 14:38:26 +0800

    提交代码

--- html/video/server.js
+++ html/video/server.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 const app = express()
 app.use(express.static('src'))
+
+app.get('/test', function(req, res) {
+    console.log('test ..........')
+    res.send({
+        msg: 'ok'
+    })
+})
+app.get('/img', function(req, res){
+    let data = fs.readFileSync(path.resolve(__dirname, 'src/logo.png'))
+    res.setHeader('Content-Type', 'image/png')
+    res.end(data)
+})
 app.listen(8888, function() {
     console.log('localhost:8888')
 })
