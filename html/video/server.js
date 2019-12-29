f1144d76e28db9aca90e3c620ac44858e15f1c20
liuchengyuan <2267698673@qq.com>
2019-12-29 10:49:49 +0800

    提交代码

--- html/video/server.js
+++ html/video/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 app.use(express.static('src'))
+
+app.get('/test', function(req,res) {
+    var data = fs.readFileSync(file_path)
+})
+
 app.listen(8888, function() {
     console.log('localhost:8888')
 })
